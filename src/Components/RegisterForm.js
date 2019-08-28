0f15cde5166abdbe6698c55475a4ae8852ca5650
Aliendreamer <dreamingman83@gmail.com>
2019-08-28 20:33:26 +0300

    ,

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import {connect} from 'react-redux';
 import classes from './../App.module.css';
 import Spinner from './Spinner';
+import { NavLink} from "react-router-dom";
 import {withFormik} from 'formik';
 import Yup from 'yup';
-class Login extends Component{
+class Register extends Component{
 
 
 
@@ -19,7 +20,7 @@ class Login extends Component{
             <input type="password" placeholder="password"/>
             <input type="text" placeholder="email address"/>
             <button>create</button>
-            <p class="message">Имаш регистрация? <a href="#">логни  се</a></p>
+            <p class="message">you have an account? <NavLink to="/login">login now</NavLink></p>
             </form>
             </div>        
         </div>
@@ -27,6 +28,12 @@ class Login extends Component{
     }
 }
 
+const mapPropsToState=state=>{
+
+    return{
+       
+    }
+}
 const mapDispatchToProps=dispatch=>{
 
     return {
@@ -34,10 +41,20 @@ const mapDispatchToProps=dispatch=>{
     }
 }
 
-export default connect(null,mapDispatchToProps)(withFormik({
+export default connect(mapPropsToState,mapDispatchToProps)(withFormik({
     mapPropsToValues(){
         return {
-        email:"testText"
+       
         }
+    }, 
+    // validationSchema:Yup.object().shape({
+    //     // username:Yup.string().required(),
+    //     // password:Yup.string()
+    //     // password:Yup.string().min(8).required()
+    // }),
+    handleSubmit(values,{props,resetForm}){
+        const{username,password}={...values} 
+        props.onSubmit(username,password);
+        resetForm()
     }
-})(Login));
+})(Register));
